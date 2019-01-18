1eb3e6656ff705286801a9c1f6abc962d70deec8
zhtzzs <zhtzzs@163.com>
2019-01-18 11:31:29 +0800

    index

--- _site/js/index.js
+++ _site/js/index.js
@@ -2,7 +2,7 @@ $(function(){
   var watch1_wd_address = 'https://weidian.com/item.html?itemID=2266200056&spider_token=af9f&spider=seller.zx-shopdetail.5.1'
   var watch1_tb_address = 'https://shop284054772.m.taobao.com/?refer=http%3A%2F%2F192.168.1.159%3A4000%2FBITHD-watch-1.html'
   var watch2_wd_address = 'https://weidian.com/item.html?itemID=2682151549'
-  var watch2_tb_address = '#'
+  var watch2_tb_address = ''
   function closeBuyBox(){
     $('.alert-buy-box').hide()
   }
